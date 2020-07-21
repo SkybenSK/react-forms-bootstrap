6448aad5755b03062f40773d7cd8427725924747
SkybenSK <it.tounsia@gmail.com>
2020-07-21 23:11:56 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div class="container">
+       <div class="row" id="content">
+         <div id="formuliare" class="col-md-6">
+          <h1>Bootstrap Forms</h1>
+          <form>
+            <div>
+              
+              <label for="inputpassword3" class="col-sm-2 col-form-label">User NAME</label>
+              <div class="col-sm-8">
+                <input tpye="text" class="form-control" id="inputpassword3" placeholder="UserName"></input>
+              </div>
+              
+              <label for="inputpassword3" class="col-sm-2 col-form-label">EMAIL</label>
+              <div class="col-sm-8">
+                <input tpye="email" class="form-control" id="inputemail3" placeholder="Email"></input>
+              </div>
+
+              <label for="inputpassword3" class="col-sm-2 col-form-label">PASSWORD</label>
+              <div class="col-sm-8">
+                <input tpye="password" class="form-control" id="inputemail3" placeholder="Password"></input>
+              </div>
+            </div>
+            <div class="form-group row">
+              <div class="col-sm-10">
+                <button type="submit" class="btn btn-primary">SIGN IN</button>
+              </div>
+            </div>
+
+            
+          </form>
+         </div>
+       </div>
+     </div>
     </div>
   );
 }
