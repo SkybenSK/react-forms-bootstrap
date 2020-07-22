5fd00fe9feff827ce0c03f415d244189408c236d
SkybenSK <it.tounsia@gmail.com>
2020-07-22 22:23:24 +0100

    newfrms

--- src/App.js
+++ src/App.js
@@ -1,43 +1,38 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <div class="container">
-       <div class="row" id="content">
-         <div id="formuliare" class="col-md-6">
-          <h1>Bootstrap Forms</h1>
-          <form>
-            <div>
-              
-              <label for="inputpassword3" class="col-sm-2 col-form-label">User NAME</label>
-              <div class="col-sm-8">
-                <input tpye="text" class="form-control" id="inputpassword3" placeholder="UserName"></input>
-              </div>
-              
-              <label for="inputpassword3" class="col-sm-2 col-form-label">EMAIL</label>
-              <div class="col-sm-8">
-                <input tpye="email" class="form-control" id="inputemail3" placeholder="Email"></input>
-              </div>
-
-              <label for="inputpassword3" class="col-sm-2 col-form-label">PASSWORD</label>
-              <div class="col-sm-8">
-                <input tpye="password" class="form-control" id="inputemail3" placeholder="Password"></input>
-              </div>
-            </div>
-            <div class="form-group row">
-              <div class="col-sm-10">
-                <button type="submit" class="btn btn-primary">SIGN IN</button>
-              </div>
-            </div>
-
-            
-          </form>
-         </div>
-       </div>
-     </div>
+      <header className="App-header">
+        <h1>CONTACT</h1>
+        <form action="#" method="post">
+          <div class="form-group">
+            <input type="text" name="first-name" id="first-name" placeholder="First name" class="form-control"></input>
+          </div>
+          <div class="form-group">
+            <input type="text" name="first-name" id="first-name" placeholder="First name" class="form-control"></input>
+          </div>
+          <div class="form-group">
+            <input type="text" name="first-name" id="first-name" placeholder="First name" class="form-control"></input>
+          </div>
+          <div class="form-group">
+            <input type="email" name="email" id="email" placeholder="Email" class="form-control"></input>
+          </div>
+          <div class="form-group">
+            <input type="tel" name="tel" id="tel" placeholder="Tel" minlength="6" maxlength="10" class="form-control"></input>
+          </div>
+          <div class="form-group">
+            <label for="message"></label>
+            <textarea name="message" id="message" cols="30" rows="10" class="form-control"></textarea>
+          </div>
+          <div class="form-group">
+            <button type="submit" class="btn btn-dark">Send</button>
+            <button type="reset" class="btn btn-light">Reset</button>
+          </div>
+        </form>
+      </header>
     </div>
   );
 }
